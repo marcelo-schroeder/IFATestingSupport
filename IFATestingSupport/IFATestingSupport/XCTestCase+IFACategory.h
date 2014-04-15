587eaaf777fc43ba07af321c62bb6ed7e5a9c6b5
Marcelo Schroeder <marcelo.schroeder@infoaccent.com>
2014-04-16 08:25:24 +1000

    Added podspec and updated classes prefix.

--- IFATestingSupport/IFATestingSupport/XCTestCase+IFACategory.h
+++ IFATestingSupport/IFATestingSupport/XCTestCase+IFACategory.h
@@ -1,15 +1,25 @@
 //
-// Created by Marcelo Schroeder on 16/08/13.
-// Copyright (c) 2013 IAG. All rights reserved.
+// Created by Marcelo Schroeder on 16/04/2014.
+// Copyright (c) 2014 InfoAccent Pty Ltd. All rights reserved.
 //
-// To change the template use AppCode | Preferences | File Templates.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 
 @class UIControl;
 @class UIBarButtonItem;
 
-@interface XCTestCase (IACategory)
+@interface XCTestCase (IFACategory)
 - (void)m_assertThatControl:(UIControl *)a_control hasTapEventConfiguredWithTarget:(id)a_target action:(SEL)a_action;
 
 - (void)m_assertThatControl:(UIControl *)a_control hasValueChangedEventConfiguredWithTarget:(id)a_target
